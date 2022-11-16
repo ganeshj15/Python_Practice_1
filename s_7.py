dd134de614dcdd25173bc543515f8cbab0bf4005
Ganesh Jambhrunkar <117712010+ganeshj15@users.noreply.github.com>
2022-11-16 09:06:22 +0530

    Add files via upload

---
+++ s_7.py
@@ -0,0 +1,7 @@
+# program to reverse a string
+s= 'python exercises'
+print("original string:", s)
+print("reversed string is:", ''.join(reversed(s)))
+     #OR # Reversal of string can be done with the following block of code.
+s= 'python exercises'
+print("reversed string is:", s[::-1])
---
+++ s_9.py
@@ -0,0 +1,4 @@
+# program to split the string on last occurrence of the delimiter/seperator
+s= 'hello,All,we,will,learn,python'
+print("original string is:", s)
+print("string after splitting", s.rsplit(',',2))
