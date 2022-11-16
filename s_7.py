de4ebbe8ad5fbfb10c5aec72603739c2ad84a598
Ganesh Jambhrunkar <117712010+ganeshj15@users.noreply.github.com>
2022-11-16 08:38:47 +0530

    Add files via upload

---
+++ s_7.py
@@ -0,0 +1,6 @@
+# program to reverse a string
+s= 'python exercises'
+print(''.join(reversed(s)))
+     #OR # Reversal of string can be done with the following block of code.
+s= 'python exercises'
+print(s[::-1])
