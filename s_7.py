86b7043fab303021c880257470aa09f7a93e313f
Ganesh Jambhrunkar <117712010+ganeshj15@users.noreply.github.com>
2022-11-16 09:05:51 +0530

    Delete s_7.py

--- s_7.py
+++
@@ -1,6 +0,0 @@
-# program to reverse a string
-s= 'python exercises'
-print("reversed string ",''.join(reversed(s)))
-     #OR # Reversal of string can be done with the following block of code.
-s= 'python exercises'
-print(s[::-1])
